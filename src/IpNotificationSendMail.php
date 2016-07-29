ba9cd63ab1cfb4a42d597fe7aef2d4c75a8d442c
Insasse <dirk.schmidt@erdfisch.de>
2016-07-29 09:29:56 +0200

    Make coder sniffer happy

--- src/IpNotificationSendMail.php
+++ src/IpNotificationSendMail.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace Drupal\ipnotification;
-use Drupal\Core\Entity\Entity;
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
 use Drupal\ipnotification\Entity\IPnotification;
 
 /**
@@ -23,9 +24,9 @@ class IpNotificationSendMail implements IpNotificationSendMailInterface {
    * Ip_check_mail_send.
    *
    * @return string
-   *
+   *    returns number of times ip matches.
    */
-  public function ip_check_mail_send($current_ip, EntityInterface $element) {
+  public function ipCheckMailSend($current_ip, EntityInterface $element) {
     global $base_url;
     $ipnotification = IPnotification::create();
     $ips = $ipnotification->getIp();
@@ -33,46 +34,56 @@ class IpNotificationSendMail implements IpNotificationSendMailInterface {
     $check = preg_match($current_ip, $ips);
 
     if ($check >= 1) {
-      $body = t('A new !element has been inserted from IP !current_ip'. $base_url . $url, array('!element' => $element->type, '!current_ip' => check_plain($current_ip))) ."\n";
-      $body .= t('To !url', array('!url' => Link::fromTextAndUrl(t('Insert'), $base_url. '/node/'. $id)));
-
+      $body = t('A new !element has been inserted from IP !current_ip !currenturl \n To !url',
+                array(
+                  '!element' => $element->id(),
+                  '!current_ip' => check_plain($current_ip),
+                  '!url' => Link::fromTextAndUrl(t('Insert'), Url::fromUri($base_url . '/node/' . $element->id())),
+                  '!currenturl' => $base_url . $url,
+                ));
       $mailkey = 'ipnotification';
       $to = $ipnotification->getEmail();
       $subject = t('New entry');
       $from = 'Community';
       $header = array('Content-Type' => 'text/html; charset=UTF-8; format=flowed');
-      \Drupal::service('plugin.manager.mail')->mail($mailkey, $to, $subject, $body, $from, $header);
+      \Drupal::service('plugin.manager.mail')->mail('ipnotification', $mailkey, $to, $subject, $body, $from, $header);
     }
     return $check;
   }
 
   /**
-   * Email_check_mail_send
+   * Email_check_mail_send.
    *
    * @return int
+   *    returns number of times ip matches.
    */
-  public function email_check_mail_send() {
+  public function emailCheckMailSend() {
     global $base_url;
-    global $user;
+    $user = \Drupal::currentUser();
     $ipnotification = IPnotification::create();
     $ips = $ipnotification->getIp();
 
     // Check which email domains where used by the user.
-    $emaildomain = explode("@", $user->mail);
+    $emaildomain = explode("@", $user->getEmail());
     $emaildomain = "%@" . $emaildomain[1];
     $check      = preg_match($emaildomain, $ips);
-    $check      = preg_match($user->mail, $ips) + $check;
+    $check      = preg_match($user->getEmail(), $ips) + $check;
 
     if ($check >= 1) {
-      $body = t('The user !username with <b>!mail</b> has logged in.<br><br>', array('!username' => $user->name, '!mail' => $user->mail)) ."\n";
-      $body .= t('To !url', array('!url' => l(t('Profile'), $base_url. '/user/'. $user->uid)));
+      $body = t('The user !username with <b>!mail</b> has logged in.<br><br> \n To !url',
+        array(
+          '!username' => $user->getDisplayName(),
+          '!mail' => $user->getEmail(),
+          '!url' => Link::fromTextAndUrl(t('Profile'), Url::fromUri($base_url . '/user/' . $user->id())),
+        ));
 
       $mailkey = 'ipnotification';
       $to = $ipnotification->getEmail();
       $subject = t('New login');
       $from = 'Community';
       $header = array('Content-Type' => 'text/html; charset=UTF-8; format=flowed');
-      \Drupal::service('plugin.manager.mail')->mail($mailkey, $to, $subject, $body, $from, $header);
+      \Drupal::service('plugin.manager.mail')->mail('ipnotification', $mailkey, $to, $subject, $body, $from, $header);
+
     }
     return $check;
   }
